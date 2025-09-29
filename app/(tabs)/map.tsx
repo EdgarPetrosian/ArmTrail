6ec73b310a0eb8c929f1f0743dfb70b8dd711eb1
Edg <edg@Edgs-MacBook-Pro.local>
2025-09-29 16:09:45 +0400

    Renders markers on the map

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,12 +1,32 @@
 import { StyleSheet } from 'react-native';
 
-import Mapbox, { Camera, LocationPuck, MapView } from '@rnmapbox/maps';
+import Mapbox, { Camera, Images, LocationPuck, MapView, ShapeSource, SymbolLayer } from '@rnmapbox/maps';
 Mapbox.setAccessToken(process.env.EXPO_PUBLIC_MAPBOX_KEY || '');
+
+import { featureCollection, point } from '@turf/helpers';
+import tree from '../../assets/images/forest.png';
+import trees from '../../data/trees.json';
+
 export default function TabThreeScreen() {
+
+  const points = trees.map(tree => point([tree.longitude, tree.latitude]))
+
   return (
     <MapView style={{ flex: 1 }} styleURL='mapbox://styles/mapbox/standard-satellite'>
       <Camera followZoomLevel={15} followUserLocation />
       <LocationPuck puckBearingEnabled puckBearing='heading' pulsing={{ isEnabled: true }} />
+      <ShapeSource id="trees" shape={featureCollection(points)} >
+        <SymbolLayer
+          id="tree-icons"
+          style={{
+            iconImage: 'tree',
+            iconSize: 0.1,
+            iconAllowOverlap: true,
+          }}
+          
+        />
+        <Images images={{ tree }} />
+      </ShapeSource>
     </MapView>
   );
 }
